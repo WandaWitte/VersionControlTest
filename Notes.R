e73fda327ca623bcbe83e4c8f7c2eac3880b7e48
WandaWitte <wanda.witte@uni-bayreuth.de>
2025-09-18 16:24:36 +0200

    Delete 1 random file, add more notes

--- Notes.R
+++ Notes.R
@@ -1,4 +1,14 @@
 ### Shortcuts
 # check Status in git panel on the right
 # tick box to add file -> prepares file to be staged
+# write commit message to keep track of changes
+# things will disappear from panel after commit
+# but reappear once new changes are saved
+# once they've been committed they are blue (modified)
+# if a committed file is deleted it will show in the Git panel too
+# files that are saved but not committed will not show if they're deleted
+# click clock symbol in git panel to see history
+# click Diff in git panel to see changes of your recent commits
+
+## COMMIT OFTEN AND USE USEFUL COMMIT MESSAGES
 
